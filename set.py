418467f113731eca4b9610bb86e6251c7cf592f1
Rishika <rishika.p1712@gmail.com>
2023-10-08 01:07:59 +0530

    setup

---
+++ set.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+name='my_ml_project',
+version='0.0.1',
+author='Rishika',
+author_email='rishika.p1712@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
